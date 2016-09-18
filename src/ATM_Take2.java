ba343dd9f27b8fd91f5c80498e686297f29a61cf
Steven Burris <stevenburris.jr@gmail.com>
2016-09-18 15:38:07 -0400

    Created a New ATM

---
+++ src/ATM_Take2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+/**
+ * Created by stevenburris on 9/18/16.
+ */
+public class ATM_Take2 {
+
+    static Scanner scanner = new Scanner(System.in);
+    static boolean newTransaction = true;
+    static HashMap<String, User> users = new HashMap<>();
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
