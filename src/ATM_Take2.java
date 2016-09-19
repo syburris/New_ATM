46fb8f059c7d6bc6206f46e2c224dd8d006c7cdd
Steven Burris <stevenburris.jr@gmail.com>
2016-09-19 13:54:22 -0400

    Created a New ATM

--- src/ATM_Take2.java
+++ src/ATM_Take2.java
@@ -8,19 +8,24 @@ public class ATM_Take2 {
 
     static HashMap<String, User> users = new HashMap<>();
     static Scanner scanner = new Scanner(System.in);
-    User steven = new User("Steven","1234",100);
 
     public static void main(String[] args) {
+        User user = new User("Steven", "1234", 100);
+        users.put(user.userName, user);
         System.out.println("Welcome to your ATM.");
         System.out.println("Please enter your name.");
         String name = scanner.nextLine();
-        if (name == null) {
+        if (users.get(name) == null) {
             System.out.println("Would you like to create an account? Type y for yes or n for no");
             String choice = scanner.nextLine();
             if (choice.equalsIgnoreCase("n")){
                 main(args);
             }
+
             else {
+                User user1 = new User(name,"1234",100);
+                users.put(name,user1);
+                System.out.printf("Hello %s !\n", users.get(name).userName);
 
             }
         }
