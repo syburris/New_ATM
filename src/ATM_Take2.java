f1e2963239851fceb23645e39a8df6551eda52cb
Steven Burris <stevenburris.jr@gmail.com>
2016-09-19 13:37:13 -0400

    Created a New ATM

--- src/ATM_Take2.java
+++ src/ATM_Take2.java
@@ -1,16 +1,33 @@
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
 import java.util.*;
 /**
  * Created by stevenburris on 9/18/16.
  */
 public class ATM_Take2 {
 
-    static Scanner scanner = new Scanner(System.in);
-    static boolean newTransaction = true;
     static HashMap<String, User> users = new HashMap<>();
+    static Scanner scanner = new Scanner(System.in);
+    User steven = new User("Steven","1234",100);
 
     public static void main(String[] args) {
+        System.out.println("Welcome to your ATM.");
+        System.out.println("Please enter your name.");
+        String name = scanner.nextLine();
+        if (name == null) {
+            System.out.println("Would you like to create an account? Type y for yes or n for no");
+            String choice = scanner.nextLine();
+            if (choice.equalsIgnoreCase("n")){
+                main(args);
+            }
+            else {
 
-        User steven = new User();
+            }
+        }
+
+
+
+
 
 
 
--- src/User.java
+++ src/User.java
@@ -1,39 +1,45 @@
-import javax.jws.soap.SOAPBinding;
-
 /**
  * Created by stevenburris on 9/18/16.
  */
 
 
 public class User {
-    String name;
+    String userName;
     String password;
     double balance;
-    String newPassword;
-    String newUser;
 
-    String getName() {
-        return name;
+    public User(String userName, String password, double balance) {
+        this.userName = userName;
+        this.password = password;
+        this.balance = balance;
     }
 
-    void setName(String newUser) throws Exception {
-        if (newUser.isEmpty()) {
-            throw new Exception("You must have a User Name!");
-        }
-        name = newUser;
-    }
+
+    //    void setName(String newUserName) throws Exception {
+//        if (newUserName.isEmpty()) {
+//            throw new Exception("You must have a User Name!");
+//        } else {
+//            userName = newUserName;
+//        }
+//    }
+//
+//    String getUserName() {
+//            return userName;
+//        }
+//
+//
+//    void setPassword(String newPassword) throws Exception {
+//        if (newPassword.isEmpty()) {
+//            throw new Exception("You have to have a password.");
+//        }
+//        else {
+//            password = newPassword;
+//        }
+//    }
 
     String getPassword() {
         return password;
     }
 
-    void setPassword() throws Exception {
-        if (newPassword.isEmpty()) {
-            throw new Exception("You have to have a password.");
-        }
-        else {
-            password = newPassword;
-        }
-    }
 }
 
